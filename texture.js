1d90d4597c914bffb70362c8b78562949a651ff9
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-08 09:36:39 -0500

    support float textures

--- texture.js
+++ texture.js
@@ -118,7 +118,7 @@ Object.defineProperties(proto, {
       this.bind()
       var gl = this.gl
       if(this.type === gl.FLOAT && linearTypes.indexOf(v) >= 0) {
-        if(!webglew(gl).OES_texture_float_linear) {
+        if(!webglew(gl).texture_float_linear) {
           v = gl.NEAREST
         }
       }
@@ -137,7 +137,7 @@ Object.defineProperties(proto, {
       this.bind()
       var gl = this.gl
       if(this.type === gl.FLOAT && linearTypes.indexOf(v) >= 0) {
-        if(!webglew(gl).OES_texture_float_linear) {
+        if(!webglew(gl).texture_float_linear) {
           v = gl.NEAREST
         }
       }
@@ -443,6 +443,9 @@ function createTextureShape(gl, width, height, format, type) {
   var maxTextureSize = gl.getParameter(gl.MAX_TEXTURE_SIZE)
   if(width < 0 || width > maxTextureSize || height < 0 || height  > maxTextureSize) {
     throw new Error('gl-texture2d: Invalid texture shape')
+  }
+  if(type === gl.FLOAT && !webglew(gl).texture_float) {
+    throw new Error('gl-texture2d: Floating point textures not supported on this platform')
   }
   var tex = initTexture(gl)
   gl.texImage2D(gl.TEXTURE_2D, 0, format, width, height, 0, format, type, null)
